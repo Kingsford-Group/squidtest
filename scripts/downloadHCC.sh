9f09ae864774cfe8d0e8ad14bd086716fce09bcb
Cong <congma@andrew.cmu.edu>
2017-07-24 16:04:15 -0400

    still checking variable naming consistency

--- scripts/downloadHCC.sh
+++ scripts/downloadHCC.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+argv=("$@")
 OutputDir=${argv[0]}
 
 mkdir -p ${OutputDir}/Ensemble75
@@ -9,10 +10,12 @@ mkdir -p ${OutputDir}/RNAseq
 cd ${OutputDir}/Ensemble75
 wget ftp://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna_sm.primary_assembly.fa.gz
 wget ftp://ftp.ensembl.org/pub/release-75/gtf/homo_sapiens/Homo_sapiens.GRCh37.75.gtf.gz
+echo "Gunzipping reference files"
 gunzip Homo_sapiens.GRCh37.75.dna_sm.primary_assembly.fa.gz
 gunzip Homo_sapiens.GRCh37.75.gtf.gz
 
 # download RNA-seq
+echo "Downloading RNA-seq fastq files"
 cd ${OutputDir}/RNAseq
 fastq-dump --split-files SRR2532344 # HCC1954
 fastq-dump --split-files SRR925710 # HCC1954
@@ -20,3 +23,8 @@ fastq-dump --split-files SRR2532336 # HCC1395
 
 cat SRR2532344_1.fastq SRR925710_1.fastq > RNAHCC1954_1.fastq
 cat SRR2532344_2.fastq SRR925710_2.fastq > RNAHCC1954_2.fastq
+
+gzip -k RNAHCC1954_1.fastq
+gzip -k RNAHCC1954_2.fastq
+gzip -k SRR2532336_1.fastq
+gzip -k SRR2532336_2.fastq
--- scripts/runHCC.sh
+++ scripts/runHCC.sh
@@ -1,101 +1,136 @@
 #!/bin/bash
 
-FusionCatcherDataDir= # reference data directory for fusioncatcher
-ChimerascanInstallDir= # chimerascan installation directory
-IntegrateInstallDir= # INTEGRATE installation directory
-OutputDir= # output files for SQUID and fusion gene detection tools on HCC1954/1395 cell lines
+argv=("$@")
+
+FusionCatcherExe=""
+FusionCatcherDataDir=""
+ChimerascanDir="" # chimerascan installation directory
+JaffaDir=""
+DefuseDir=""
+PicardDir=""
+OutputDir="" # downloaded reference and RNA-seq folder, the same as output folder
+DataHCC=""
+threads=2
 ExeDir=$(pwd)
 
+for ((i=0; i<${#argv}; i+=2)); do
+	if [[ ${argv[$i]} == "--fusioncatcher" ]]; then
+		FusionCatcherExe=${argv[(($i+1))]}
+	elif [[ ${argv[$i]} == "--fusioncatcherdata" ]]; then
+		FusionCatcherDataDir=${argv[(($i+1))]}
+	elif [[ ${argv[$i]} == "--chimeriscan" ]]; then
+		ChimerascanDir=${argv[(($i+1))]}
+	elif [[ ${argv[$i]} == "--jaffa" ]]; then
+		JaffaDir=${argv[(($i+1))]}
+	elif [[ ${argv[$i]} == "--defuse" ]]; then
+		DefuseDir=${argv[(($i+1))]}
+	elif [[ ${argv[$i]} == "--picard" ]]; then
+		PicardDir=${argv[(($i+1))]}
+	elif [[ ${argv[$i]} == "--datadownload" ]]; then
+		OutputDir=${argv[(($i+1))]}
+	elif [[ ${argv[$i]} == "--dataHCC" ]]; then
+		DataHCC=${argv[(($i+1))]}
+	elif [[ ${argv[$i]} == "--threads" ]]; then
+		threads=${argv[(($i+1))]}
+	fi
+done
+
+echo "INTEGRATE executable are in your path."
+
 GenomeFasta=${OutputDir}/Ensemble75/Homo_sapiens.GRCh37.75.dna_sm.primary_assembly.fa
-AnnotationGTF=${OutputDir}/Ensemble75/Homo_sapiens.GRCh37.75.gtf.gz
+AnnotationGTF=${OutputDir}/Ensemble75/Homo_sapiens.GRCh37.75.gtf
 
 # preparing STAR index
-mkdir -p ${GenomeFasta%/*}/STARIndex
-STAR --runThreadN 12 --runMode genomeGenerate --genomeDir ${GenomeFasta%/*}/STARIndex --genomeFastaFiles ${GenomeFasta} --sjdbGTFfile ${AnnotationGTF}
+echo "Generating STAR index"
+mkdir -p ${OutputDir}/Ensemble75/STARIndex
+STAR --runThreadN ${threads} --runMode genomeGenerate --genomeDir ${OutputDir}/Ensemble75/STARIndex --genomeFastaFiles ${GenomeFasta} --sjdbGTFfile ${AnnotationGTF}
 # preparing chimerascan Bowtie index
+echo "Generating chimerascan index"
 mkdir -p ${OutputDir}/Ensemble75/CSBowtieIndex
-gunzip ${ExeDir}/dataHCC/Homo_sapiens.GRCh37.75_chimerascan.txt.gz
-python ${ChimerascanInstallDir}/bin/chimerascan_index.py ${GenomeFasta} ${ExeDir}/dataHCC/Homo_sapiens.GRCh37.75_chimerascan.txt.gz ${OutputDir}/Ensemble75/CSBowtieIndex
+gunzip ${DataHCC}/Homo_sapiens.GRCh37.75_chimerascan.txt.gz
+python ${ChimerascanDir}/bin/chimerascan_index.py ${GenomeFasta} ${DataHCC}/Homo_sapiens.GRCh37.75_chimerascan.txt ${OutputDir}/Ensemble75/CSBowtieIndex
 # preparing INTEGRATE index
-gunzip ${ExeDir}/dataHCC/annot.ensembl.txt.gz
+echo "Generating INTEGRATE index"
+gunzip ${DataHCC}/annot.ensembl.txt.gz
 mkdir -p ${OutputDir}/Ensemble75/IntegrateIndex
 ${IntegrateIntallDir}/bin/Integrate mkbwt -dir ${OutputDir}/Ensemble75/IntegrateIndex ${GenomeFasta}
 
 cd $OutputDir
 # align reads using STAR
-mkdir -p StarAlign_1954
-STAR  --runThreadN 12 --genomeDir ${GenomeFasta%/*}/STARIndex --readFilesIn ${ExeDir}/dataHCC/RNAHCC1954_1.fastq ${ExeDir}/dataHCC/RNAHCC1954_2.fastq --outFileNamePrefix $OutputDir/StarAlign_1954/ --outSAMtype BAM SortedByCoordinate --outSAMstrandField intronMotif --chimSegmentMin 15 --outReadsUnmapped Fastx
-samtools view -Shb StarAlign_1954/Chimeric.out.sam -o StarAlign_1954/Chimeric.out.bam
+echo "STAR alignment"
+mkdir -p ${OutputDir}/StarAlign/HCC1954
+STAR  --runThreadN ${threads} --genomeDir ${OutputDir}/Ensemble75/STARIndex --readFilesIn ${OutputDir}/RNAseq/RNAHCC1954_1.fastq ${OutputDir}/RNAseq/RNAHCC1954_2.fastq --outFileNamePrefix $OutputDir/StarAlign/HCC1954/ --outSAMtype BAM SortedByCoordinate --outSAMstrandField intronMotif --chimSegmentMin 15 --outReadsUnmapped Fastx
+samtools view -Shb ${OutputDir}/StarAlign/HCC1954/Chimeric.out.sam -o ${OutputDir}/StarAlign/HCC1954/Chimeric.out.bam
 
-mkdir -p StarAlign_1395
-STAR  --runThreadN 12 --genomeDir ${GenomeFasta%/*}/STARIndex --readFilesIn ${ExeDir}/dataHCC/SRR2532336_1.fastq ${ExeDir}/dataHCC/SRR2532336_2.fastq --outFileNamePrefix $OutputDir/StarAlign_1395/ --outSAMtype BAM SortedByCoordinate --outSAMstrandField intronMotif --chimSegmentMin 15 --outReadsUnmapped Fastx
-samtools view -Shb StarAlign_1395/Chimeric.out.sam -o StarAlign_1395/Chimeric.out.bam
+mkdir -p ${OutputDir}/StarAlign/HCC1395
+STAR  --runThreadN ${threads} --genomeDir ${OutputDir}/Ensemble75/STARIndex --readFilesIn ${OutputDir}/RNAseq/SRR2532336_1.fastq ${OutputDir}/RNAseq/SRR2532336_2.fastq --outFileNamePrefix $OutputDir/StarAlign/HCC1395/ --outSAMtype BAM SortedByCoordinate --outSAMstrandField intronMotif --chimSegmentMin 15 --outReadsUnmapped Fastx
+samtools view -Shb ${OutputDir}/StarAlign/HCC1395/Chimeric.out.sam -o ${OutputDir}/StarAlign/HCC1395/Chimeric.out.bam
 
 # SQUID
-mkdir -p squid_1954
-squid -b StarAlign_1954/Aligned.sortedByCoord.out.bam -c StarAlign_1954/Chimeric.out.bam -o squid_1954/squidtsv
-awk 'BEGIN{FS="\t";} {if(substr($0,0,1)=="#" || !($1~"M" || $1~"G" || $4~"M" || $4~"G")) print $0;}' squid_1954/squidtsv_sv.txt > squid_1954/squidtsv_sv_final.txt
-python3 VerifyFusionGene.py
+echo "predicting TSV with SQUID"
+mkdir -p ${OutputDir}/TSVprediction/squid_1954
+squid -b ${OutputDir}/StarAlign/HCC1954/Aligned.sortedByCoord.out.bam -c ${OutputDir}/StarAlign/HCC1954/Chimeric.out.bam -o ${OutputDir}/TSVprediction/squid_1954/squidtsv
+awk 'BEGIN{FS="\t";} {if(substr($0,0,1)=="#" || !($1~"M" || $1~"G" || $4~"M" || $4~"G")) print $0;}' ${OutputDir}/TSVprediction/squid_1954/squidtsv_sv.txt > ${OutputDir}/TSVprediction/squid_1954/squidtsv_sv_final.txt
 
-mkdir squid_1395
-squid -b StarAlign_1395/Aligned.sortedByCoord.out.bam -c StarAlign_1395/Chimeric.out.bam -o squid_1395/squidtsv
-awk 'BEGIN{FS="\t";} {if(substr($0,0,1)=="#" || !($1~"M" || $1~"G" || $4~"M" || $4~"G")) print $0;}' squid_1395/squidtsv_sv.txt > squid_1395/squidtsv_sv_final.txt
+mkdir -p ${OutputDir}/TSVprediction/squid_1395
+squid -b ${OutputDir}/StarAlign/HCC1395/Aligned.sortedByCoord.out.bam -c ${OutputDir}/StarAlign/HCC1395/Chimeric.out.bam -o ${OutputDir}/TSVprediction/squid_1395/squidtsv
+awk 'BEGIN{FS="\t";} {if(substr($0,0,1)=="#" || !($1~"M" || $1~"G" || $4~"M" || $4~"G")) print $0;}' ${OutputDir}/TSVprediction/squid_1395/squidtsv_sv.txt > ${OutputDir}/TSVprediction/squid_1395/squidtsv_sv_final.txt
 
 # fusioncatcher
-mkdir -p fusioncatcher_1954
-mkdir -p fusioncatcher_1954/data
-ln -s ${ExeDir}/dataHCC/RNAHCC1954_1.fastq fusioncatcher_1954/data/
-ln -s ${ExeDir}/dataHCC/RNAHCC1954_2.fastq fusioncatcher_1954/data/
-fusioncatcher -d ${FusionCatcherDataDir} -i fusioncatcher_1954/data/ -o fusioncatcher_1954/
+mkdir -p ${OutputDir}/TSVprediction/fusioncatcher_1954
+mkdir -p ${OutputDir}/TSVprediction/fusioncatcher_1954/data
+ln -s ${OutputDir}/RNAseq/RNAHCC1954_1.fastq ${OutputDir}/TSVprediction/fusioncatcher_1954/data/
+ln -s ${OutputDir}/RNAseq/RNAHCC1954_2.fastq ${OutputDir}/TSVprediction/fusioncatcher_1954/data/
+${FusionCatcherExe} -d ${FusionCatcherDataDir} -i ${OutputDir}/TSVprediction/fusioncatcher_1954/data/ -o ${OutputDir}/TSVprediction/fusioncatcher_1954/
 
-mkdir -p fusioncatcher_1395
-mkdir -p fusioncatcher_1395/data
-ln -s ${ExeDir}/dataHCC/SRR2532336_1.fastq fusioncatcher_1395/data/
-ln -s ${ExeDir}/dataHCC/SRR2532336_2.fastq fusioncatcher_1395/data/
-fusioncatcher -d ${FusionCatcherDataDir} -i fusioncatcher_1395/data/ -o fusioncatcher_1395/
+mkdir -p ${OutputDir}/TSVprediction/fusioncatcher_1395
+mkdir -p ${OutputDir}/TSVprediction/fusioncatcher_1395/data
+ln -s ${OutputDir}/RNAseq/SRR2532336_1.fastq ${OutputDir}/TSVprediction/fusioncatcher_1395/data/
+ln -s ${OutputDir}/RNAseq/SRR2532336_2.fastq ${OutputDir}/TSVprediction/fusioncatcher_1395/data/
+${FusionCatcherExe} -d ${FusionCatcherDataDir} -i ${OutputDir}/TSVprediction/fusioncatcher_1395/data/ -o ${OutputDir}/TSVprediction/fusioncatcher_1395/
 
 # JAFFA
-mkdir -p jaffa_1954
-mkdir -p jaffa_1954/data
-ln -s ${ExeDir}/dataHCC/RNAHCC1954_1.fastq jaffa_1954/data
-ln -s ${ExeDir}/dataHCC/RNAHCC1954_2.fastq jaffa_1954/data
-cd jaffa_1954
-${JaffaDir}/tools/bin/bpipe run ${JaffaDir}/JAFFA_assembly.groovy jaffa_1954/data/RNAHCC1954_*.fastq
-cd ..
+mkdir -p ${OutputDir}/TSVprediction/jaffa_1954
+mkdir -p ${OutputDir}/TSVprediction/jaffa_1954/data
+ln -s ${OutputDir}/RNAseq/RNAHCC1954_1.fastq.gz ${OutputDir}/TSVprediction/jaffa_1954/data
+ln -s ${OutputDir}/RNAseq/RNAHCC1954_2.fastq.gz ${OutputDir}/TSVprediction/jaffa_1954/data
+cd ${OutputDir}/TSVprediction/jaffa_1954
+${JaffaDir}/tools/bin/bpipe run ${JaffaDir}/JAFFA_assembly.groovy ./data/RNAHCC1954_*.fastq.gz
+cd ${ExeDir}
 
-mkdir -p jaffa_1395
-mkdir -p jaffa_1395/data
-ln -s ${ExeDir}/dataHCC/SRR2532336_1.fastq jaffa_1395/data/
-ln -s ${ExeDir}/dataHCC/SRR2532336_2.fastq jaffa_1395/data/
-cd jaffa_1395
-${JaffaDir}/tools/bin/bpipe run ${JaffaDir}/JAFFA_assembly.groovy jaffa_1395/data/SRR2532336_*.fastq
-cd ..
+mkdir -p ${OutputDir}/TSVprediction/jaffa_1395
+mkdir -p ${OutputDir}/TSVprediction/jaffa_1395/data
+ln -s ${OutputDir}/RNAseq/SRR2532336_1.fastq.gz ${OutputDir}/TSVprediction/jaffa_1395/data/
+ln -s ${OutputDir}/RNAseq/SRR2532336_2.fastq.gz ${OutputDir}/TSVprediction/jaffa_1395/data/
+cd ${OutputDir}/TSVprediction/jaffa_1395
+${JaffaDir}/tools/bin/bpipe run ${JaffaDir}/JAFFA_assembly.groovy ./data/SRR2532336_*.fastq.gz
+cd ${ExeDir}
 
 # deFuse
-mkdir -p defuse_1954
-${DefuseDir}/scripts/defuse_run.pl -c ${DefuseDir}/scripts/config.txt -d ${DefuseDir}/RefData/ -1 ${ExeDir}/dataHCC/RNAHCC1954_1.fastq -2 ${ExeDir}/dataHCC/RNAHCC1954_2.fastq -o defuse_1954 -p 4
+mkdir -p ${OutputDir}/TSVprediction/defuse_1954
+${DefuseDir}/scripts/defuse_run.pl -c ${DefuseDir}/scripts/config.txt -d ${DefuseDir}/RefData/ -1 ${OutputDir}/RNAseq/RNAHCC1954_1.fastq -2 ${OutputDir}/RNAseq/RNAHCC1954_2.fastq -o ${OutputDir}/TSVprediction/defuse_1954 -p ${threads}
 
-mkdir -p defuse_1395
-${DefuseDir}/scripts/defuse_run.pl -c ${DefuseDir}/scripts/config.txt -d ${DefuseDir}/RefData/ -1 ${ExeDir}/dataHCC/SRR2532336_1.fastq -2 ${ExeDir}/dataHCC/SRR2532336_2.fastq -o defuse_1954 -p 4
+mkdir -p ${OutputDir}/TSVprediction/defuse_1395
+${DefuseDir}/scripts/defuse_run.pl -c ${DefuseDir}/scripts/config.txt -d ${DefuseDir}/RefData/ -1 ${OutputDir}/RNAseq/SRR2532336_1.fastq -2 ${OutputDir}/RNAseq/SRR2532336_2.fastq -o ${OutputDir}/TSVprediction/defuse_1954 -p ${threads}
 
 # Chimerascan
-mkdir -p chimerascan_1954
-python ${ChimerascanDir}/bin/chimerascan_run.py -v -p 4 ${OutputDir}/Ensemble75/CSBowtieIndex ${ExeDir}/dataHCC/RNAHCC1954_1.fastq ${ExeDir}/dataHCC/RNAHCC1954_2.fastq chimerascan_1954/
+mkdir -p ${OutputDir}/TSVprediction/chimerascan_1954
+python ${ChimerascanDir}/bin/chimerascan_run.py -v -p ${threads} ${OutputDir}/Ensemble75/CSBowtieIndex ${OutputDir}/RNAseq/RNAHCC1954_1.fastq ${OutputDir}/RNAseq/RNAHCC1954_2.fastq ${OutputDir}/TSVprediction/chimerascan_1954/
 
-mkdir chimerascan_1395
-python ${ChimerascanDir}/bin/chimerascan_run.py -v -p 4 ${OutputDir}/Ensemble75/CSBowtieIndex ${ExeDir}/dataHCC/SRR2532336_1.fastq ${ExeDir}/dataHCC/SRR2532336_2.fastq chimerascan_1395/
+mkdir -p ${OutputDir}/TSVprediction/chimerascan_1395
+python ${ChimerascanDir}/bin/chimerascan_run.py -v -p ${threads} ${OutputDir}/Ensemble75/CSBowtieIndex ${OutputDir}/RNAseq/SRR2532336_1.fastq ${OutputDir}/RNAseq/SRR2532336_2.fastq ${OutputDir}/TSVprediction/chimerascan_1395/
 
 # INTEGRATE
-samtools merge $OutputDir/StarAlign_1954/MergedAlign.bam $OutputDir/StarAlign_1954/Aligned.sortedByCoord.out.bam $OutputDir/StarAlign_1954/Chimeric.out.bam
-samtools sort $OutputDir/StarAlign_1954/MergedAlign.bam -o $OutputDir/StarAlign_1954/MergedAlign_sort.bam
-samtools index $OutputDir/StarAlign_1954/MergedAlign_sort.bam
-java -jar ${}/picard-jar FastqToSam F1=$OutputDir/StarAlign_1954/Unmapped.out.mate1 F2=$OutputDir/StarAlign_1954/Unmapped.out.mate2 O=$OutputDir/StarAlign_1954/Unmapped.bam SM=HCC1954 
-mkdir -p integrate_1954
-${IntegrateIntallDir}/bin/Integrate fusion ${GenomeFasta} ${ExeDir}/dataHCC/annot.ensembl.txt ${OutputDir}/Ensemble75/IntegrateIndex $OutputDir/StarAlign_1954//MergedAlign_sort.bam $OutputDir/StarAlign_1954/Unmapped.bam
+samtools merge $OutputDir/StarAlign/HCC1954/MergedAlign.bam $OutputDir/StarAlign/HCC1954/Aligned.sortedByCoord.out.bam $OutputDir/StarAlign/HCC1954/Chimeric.out.bam
+samtools sort $OutputDir/StarAlign/HCC1954/MergedAlign.bam -o $OutputDir/StarAlign/HCC1954/MergedAlign_sort.bam
+samtools index $OutputDir/StarAlign/HCC1954/MergedAlign_sort.bam
+java -jar ${PicardDir}/picard.jar FastqToSam F1=$OutputDir/StarAlign/HCC1954/Unmapped.out.mate1 F2=$OutputDir/StarAlign/HCC1954/Unmapped.out.mate2 O=$OutputDir/StarAlign/HCC1954/Unmapped.bam SM=HCC1954 
+mkdir -p ${OutputDir}/TSVprediction/integrate_1954
+Integrate fusion ${GenomeFasta} ${DataHCC}/annot.ensembl.txt ${OutputDir}/Ensemble75/IntegrateIndex $OutputDir/StarAlign/HCC1954//MergedAlign_sort.bam $OutputDir/StarAlign/HCC1954/Unmapped.bam
 
-samtools merge $OutputDir/StarAlign_1395/MergedAlign.bam $OutputDir/StarAlign_1395/Aligned.sortedByCoord.out.bam $OutputDir/StarAlign_1395/Chimeric.out.bam
-samtools sort $OutputDir/StarAlign_1395/MergedAlign.bam -o $OutputDir/StarAlign_1395/MergedAlign_sort.bam
-samtools index $OutputDir/StarAlign_1395/MergedAlign_sort.bam
-java -jar ${}/picard-jar FastqToSam F1=$OutputDir/StarAlign_1395/Unmapped.out.mate1 F2=$OutputDir/StarAlign_1395/Unmapped.out.mate2 O=$OutputDir/StarAlign_1395/Unmapped.bam SM=HCC1395
-mkdir -p integrate_1395
-${IntegrateIntallDir}/bin/Integrate fusion ${GenomeFasta} ${ExeDir}/dataHCC/annot.ensembl.txt ${OutputDir}/Ensemble75/IntegrateIndex $OutputDir/StarAlign_1395//MergedAlign_sort.bam $OutputDir/StarAlign_1395/Unmapped.bam
+samtools merge $OutputDir/StarAlign/HCC1395/MergedAlign.bam $OutputDir/StarAlign/HCC1395/Aligned.sortedByCoord.out.bam $OutputDir/StarAlign/HCC1395/Chimeric.out.bam
+samtools sort $OutputDir/StarAlign/HCC1395/MergedAlign.bam -o $OutputDir/StarAlign/HCC1395/MergedAlign_sort.bam
+samtools index $OutputDir/StarAlign/HCC1395/MergedAlign_sort.bam
+java -jar ${PicardDir}/picard.jar FastqToSam F1=$OutputDir/StarAlign/HCC1395/Unmapped.out.mate1 F2=$OutputDir/StarAlign/HCC1395/Unmapped.out.mate2 O=$OutputDir/StarAlign/HCC1395/Unmapped.bam SM=HCC1395
+mkdir -p ${OutputDir}/TSVprediction/integrate_1395
+Integrate fusion ${GenomeFasta} ${DataHCC}/annot.ensembl.txt ${OutputDir}/Ensemble75/IntegrateIndex $OutputDir/StarAlign/HCC1395//MergedAlign_sort.bam $OutputDir/StarAlign/HCC1395/Unmapped.bam
